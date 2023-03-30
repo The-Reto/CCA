c53782e8a89b2838a835b683eb7881fdde87e181
Reto Fluetsch <rfluetsch@ethz.ch>
2023-03-30 13:39:54 +0200

    cleaned up test

--- test.cpp
+++ test.cpp
@@ -2,49 +2,40 @@
 #include "GOL_RNG.h"
 
 int test(int seed) {
-    static const int range = 10;
+    static const int range = 20, avg=100;
     GOL_RNG test(seed);
     
-    int samples = range*10000;
+    int samples = range*avg;
     int results[range] = {0};
     
     for (int i = 0; i < samples; i++) {
-        int index = std::floor(test.rand_float() * range);
-        results[index]++;
+        //results[(int) std::floor(test.rand_double() * range)]++;
+        results[test.rand_int(range)]++;
     }
     for (int i =0; i < range; i++) {
         std::cout << results[i] << " ";
-        if (results[i] < 100000) {std::cout << " ";}
+        if (results[i] < avg) {std::cout << " ";}
     }
     return test.rand_int();
 }
 
-void demo() {
-    GOL_RNG a(3141591), b(22051996), c(16091999);
-    for (int i = 0; i < 10; i++) {
-        std::cout << a.rand_int(100) << " " << b.rand_int(100) << " " << c.rand_int(100) << " " << std::endl;
+int demo(unsigned int seed) {
+    GOL_RNG a(seed ^ 3141591);
+    for (int i = 0; i < 15; i++) {
+    	std::cout << a.rand_double() << " ";
     }
+    return a.rand_int();
 }
 
 int main()
 {
     int seed = 22051996;
     
-    //demo();
+    for(int i = 0; i < 10; i++) {
+        seed = test(seed);
+        //seed = demo(seed);
+        std::cout << std::endl;
+    }
     
-    GOL_CRYPTO test(24,24,2048+1024+512);
-    test.print();
-    test.step();
-    test.print();
-    test.step();
-    test.print();
-    test.step();
-    test.print();
-    
-    //for(int i = 0; i < 5; i++) {
-    //    seed = test(seed);
-    //    std::cout << std::endl;
-    //}
-    //demo();
 
 }
