35922b42fb00d43923d7dff74ab78bbb4bb84d4f
Reto Fluetsch <rfluetsch@ethz.ch>
2023-04-08 17:08:16 +0200

    added more tests

---
+++ Test_CGOL.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include "./headers/GOL_Crypto.h"
+
+void test_GOL() {
+    GOL_CRYPTO test(32,32,0xefecafe1);
+    system("clear");
+    
+    std::cout << "C-GOL Simulation \t Initial configuration" << "\n";
+    test.print();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    system("clear");
+    
+    for (int i = 1; i<200; ++i) {
+        std::cout << "C-GOL Simulation \t Generation: " << i << "\n";
+        test.print();
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+        test.step();
+        system("clear");
+    }
+    
+    std::cout << std::endl;
+    system("clear");
+}
+
+int main()
+{
+    test_GOL();
+
+}
