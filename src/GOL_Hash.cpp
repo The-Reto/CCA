3fd458c6c9bb23c62c50dd5d4d2fbc7b1580dcc0
The-Reto <fluetsch@the-reto.ch>
2024-03-04 17:29:30 +0100

    Fixed mistake in bitshift while folding

--- src/GOL_Hash.cpp
+++ src/GOL_Hash.cpp
@@ -7,7 +7,7 @@ GOL_Hash::GOL_Hash(std::string _path) : gol_board(0), input_stream(_path), hashe
 void GOL_Hash::fold() {
     for (int i =  0; i < SIZE_Y/2; i++) {
         gol_board.board[0][i] ^= gol_board.board[0][SIZE_Y-1-i];
-        gol_board.board[0][i] ^= gol_board.board[0][i] << (SIZE_X /2);
+        gol_board.board[0][i] ^= gol_board.board[0][i] >> (SIZE_X /2);
     }
 }
 
