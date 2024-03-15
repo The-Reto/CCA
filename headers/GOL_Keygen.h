e0a99a703b39dc41c0efe15d336c2acd96b89fd7
The-Reto <fluetsch@the-reto.ch>
2024-03-15 15:35:39 +0100

    Updated GOL_Keygen

--- headers/GOL_Keygen.h
+++ headers/GOL_Keygen.h
@@ -6,7 +6,7 @@
 class GOL_Keygen {
     
     Cryptographic_GOL_Board gol_board;
-    boost::dynamic_bitset<unsigned char> key, temp;
+    Bit_Board<u_int64_t> key, temp;
     const static short SIZE_X = 64, SIZE_Y = 64, BLOCK_STEPS = 2;
     
     void setup();
@@ -14,7 +14,7 @@ class GOL_Keygen {
     public:
     GOL_Keygen(std::string key_);
     
-    boost::dynamic_bitset<unsigned char> get_streched_key(unsigned long len);
+    Bit_Board<u_int64_t>  get_streched_key();
     
 };
 #endif
--- src/GOL_Keygen.cpp
+++ src/GOL_Keygen.cpp
@@ -1,34 +1,21 @@
 #include "../headers/GOL_Keygen.h"
 
 GOL_Keygen::GOL_Keygen(std::string key_) : gol_board(0) {
-    for (unsigned char c : key_) { key.append(c);}
-    setup();
-}
-
-void GOL_Keygen::setup() {
     u_int64_t seed = 0;
-    std::string str;
-    boost::to_string(key, str);
-    for (int i = 0; i < str.length(); i++) {
-        seed += ((u_int64_t) str[i]) << (8*i % 64);
+    for (int i = 0; i < key_.length(); i++) {
+        seed += ((u_int64_t) key_[i]) << (8*i % 64);
     }
     gol_board.set_seed(seed);
     gol_board.create_seed_map();
     gol_board.steps(SIZE_X);
 }
 
-boost::dynamic_bitset<unsigned char> GOL_Keygen::get_streched_key(unsigned long len) { 
-    key = gol_board.dynamic_rand_bits<SIZE_X * SIZE_X /2>();
-    if (len > SIZE_X * SIZE_X /2) {
-        key.resize(len);
-        boost::dynamic_bitset<unsigned char> temp;
-        for (int i = SIZE_X * SIZE_X /2; i < len; i++) {
-            if (i % SIZE_X * SIZE_X /2 == 0) {
-                temp = gol_board.dynamic_rand_bits<SIZE_X * SIZE_X /2>();
-            }
-            key.set(i, temp[ i % (SIZE_X*SIZE_Y) ]);
-        }
-    }
-    else if (len < SIZE_X * SIZE_X /2) { key.resize(len); }
+void GOL_Keygen::setup() {
+
+}
+
+Bit_Board<u_int64_t> GOL_Keygen::get_streched_key() { 
+    key = gol_board[0];
+    gol_board.step();
     return key;
 }
