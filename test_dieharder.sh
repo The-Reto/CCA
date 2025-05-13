3cce393dfdb21584ebb5fbe1f25c224e20a15deb
The-Reto <fluetsch@the-reto.ch>
2025-05-13 16:18:40 +0200

    test_dieharder now tests xor of two similar keys

--- test_dieharder.sh
+++ test_dieharder.sh
@@ -2,7 +2,7 @@
 
 for d in {1..10}; do
     seed=$(cksum <<< "$(date)" | cut -f 1 -d ' ')
-    touch ./Documentation/Validation/"${seed}".txt
+    touch ./Documentation/Validation/"two_keys_${seed}".txt
     echo "$d / 10"
-    ./execs/tests/Test_Dieharder.run "${seed}" | dieharder -a -g 200 > ./Documentation/Validation/"${seed}".txt
+    ./execs/tests/Test_Dieharder.run "${seed}" | dieharder -a -g 200 > ./Documentation/Validation/"two_keys_${seed}".txt
 done
--- tests/Test_Dieharder.cpp
+++ tests/Test_Dieharder.cpp
@@ -5,7 +5,12 @@ int main(int argc, char *argv[])
 {
     CCA_Key test_a = CCA_Key::generate_key(argv[1]);
     int res = 0;
+    Bit_Board<u_int64_t> out = test_a.get_board();
+    out[0] ^= 0x10000000;
+    CCA_Key test_b(out);
+    out.reset();
     while (true) {
-         res = write(1,&test_a.get_streched_key()[0],512);
+        out = *test_a.get_streched_key() ^ *test_b.get_streched_key();
+        res = write(1,&out,512);
     }
 }
